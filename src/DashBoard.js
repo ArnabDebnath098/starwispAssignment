78808fa4a6d67681e468adf4e45ab2225fd7c0ff
ArnabDebnath <48032356+ArnabDebnath098@users.noreply.github.com>
2020-11-29 14:01:43 +0530

    Update DashBoard.js

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -67,13 +67,13 @@ function DashBoard() {
 
             <div className="dashBoard-left">
                 <Link to="/">
-                   <a href="#" className="dashboardKey"> <DashboardIcon className="left-Icon"/> Dashboard</a>
+                   <a href="address.html" className="dashboardKey"> <DashboardIcon className="left-Icon"/> Dashboard</a>
                 </Link>
                 
-                <a href="#" className="dashboardKey"> <SchoolIcon className="left-Icon"/> Lorem</a>
-                <a href="#" className="dashboardKey"> <SettingsIcon className="left-Icon"/> Ipsum</a>
+                <a href="address.html" className="dashboardKey"> <SchoolIcon className="left-Icon"/> Lorem</a>
+                <a href="address.html" className="dashboardKey"> <SettingsIcon className="left-Icon"/> Ipsum</a>
                 <Link to="/posts">
-                    <a href="#" className="dashboardKey"> <PostAddIcon className="left-Icon"/> Posts</a>
+                    <a href="address.html" className="dashboardKey"> <PostAddIcon className="left-Icon"/> Posts</a>
                 </Link>
                
             </div>
